d16179319760decf2eded83d994b06ea56811c30
Pelayo Alvarez Garcia <pelayoalvarez11@gmail.com>
2025-12-03 00:01:47 +0100

    Application Unit Test
    Application usecases unit test impl

---
+++ similar-products/src/test/java/com/inditex/challenge/application/GetSimilarProductsUseCaseImplTest.java
@@ -0,0 +1,46 @@
+package com.inditex.challenge.application;
+
+import com.inditex.challenge.domain.model.Product;
+import com.inditex.challenge.domain.model.vo.ProductId;
+import com.inditex.challenge.domain.port.in.GetProductDetailUseCase;
+import com.inditex.challenge.domain.port.out.SimilarProductsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetSimilarProductsUseCaseImplTest {
+
+    @InjectMocks
+    private GetSimilarProductsUseCaseImpl useCase;
+    @Mock
+    private SimilarProductsRepository repository;
+    @Mock
+    private GetProductDetailUseCase getProductDetailUseCase;
+    @Mock
+    private ProductId productId;
+    @Mock
+    private Product product;
+
+    @Test
+    void execute() {
+        when(repository.findSimilarIds(productId)).thenReturn(Set.of(productId));
+        when(getProductDetailUseCase.execute(productId)).thenReturn(product);
+        final var result = useCase.execute(productId);
+        assertAll(
+                () -> assertNotNull(result),
+                () -> assertNotNull(result.products()),
+                () -> assertFalse(result.products().isEmpty()),
+                () -> assertTrue(result.products().contains(product))
+        );
+        verify(repository, times(1)).findSimilarIds(productId);
+        verify(getProductDetailUseCase, times(1)).execute(productId);
+    }
+}
